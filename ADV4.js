7b1390ee38b66c272a437af83ff009545ba3d9de
Quan <quanigneer@gmail.com>
2022-05-21 08:01:38 -0700

    ADV4.js

--- ADV4.js
+++ ADV4.js
@@ -9,11 +9,62 @@ document.getElementById('inputfile').addEventListener('change', function () {
 })
 
 
-function solution (text){
-    console.log(text);
-    console.log(typeof text);
-    console.log(text[0]);
-    console.log(typeof text[0]);
+function solution(text) {
+    let text1 = text.split("\n");
+    let inputs = text1[0].split(",");
+    text1 = text1.slice(2);
+    console.log(text1);
 
-
+    let matrices = new Array();
+    let matrix = new Array();
+    for (i = 0; i < text1.length; i++) {
+        // Split each line into row and convert each element to Integer.
+        let row = text1[i].replace(/\s+/g, " ").split(" ");
+        for (j = 0; j < row.length; j++) {
+            row[j] = parseInt(row[j]);
+        }
+        // Combine 5 lines into a matrix and add the matrix into matrices.
+        if (i % 6 == 5) {
+            matrices.push(matrix);
+            matrix = new Array();
+        } else {
+            matrix.push(row);
+        }
+    }
+    console.log(matrices);
 }
+
+
+// Return true if m wins, else return false.
+// m is a 5x5 matrix.
+// For example:
+// [
+//      [56, -1, 70, 2, 22]
+//      [44, -1, 10, -1, 8]
+//      [92, -1, -1, -1, 93]
+//      [74, -1, 5, -1, 68]
+//      [24, -1, 42, 65, 72]
+// ]
+function verify(m) {
+    for (x = 0; x < m.length; x++ ){ // loop row
+        let isRowWin = true;
+        for (y = 0; y < m[x].length; y++){ // loop elements in row
+            if (m[x][y] != -1) {
+                isRowWin = false;
+                break;
+            }
+        }
+        if (isRowWin) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(verify([
+     [44, -1, 10, -1, 8],
+     [92, -1, -1, -1, 93],
+     [74, -1, 5, -1, 68],
+     [24, -1, 42, 65, 72],
+     [-1, -1, -1, -1, -1],
+]))
